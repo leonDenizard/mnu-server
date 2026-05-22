9a8d8496676058907fb9881c9b2dd59519aacd2d
leonDenizard <de.nizard@hotmail.com>
2026-05-21 23:16:04 -0300

    feat(modifierOptions): add schemas at contract on api modifierOptions

---
+++ src/modules/menu/modifierGroups/modifierOptions/modifierOptions.schema.ts
@@ -0,0 +1,50 @@
+import z from "zod";
+
+const modifierOptionItemSchema = z.object({
+    name: z.string().min(3),
+    description: z.string().max(300).optional().nullable(),
+    image: z.string().optional(),
+    price: z.number().optional(),
+    maxQuantity: z.number().int().optional(),
+    active: z.boolean().optional(),
+    displayOrder: z.number().int().optional(),
+})
+
+const modifierOptionsInputSchema = z.object({
+    data: z.array(modifierOptionItemSchema).min(1)
+})
+
+const modifierOptionsOutputSchema = z.object({
+    id: z.string().uuid(),
+    name: z.string().min(3),
+    description: z.string().max(300),
+    image: z.string(),
+    price: z.number(),
+    maxQuantity: z.number(),
+    active: z.boolean(),
+    displayOrder: z.number().int(),
+    createdAt: z.string(),
+    updatedAt: z.string(),
+    modifierGroupId: z.string().uuid()
+})
+
+export const modifierOptionsResponseListSchema = z.object({
+    success: z.literal(true),
+    data: z.array(modifierOptionsOutputSchema)
+})
+
+export const modifierOptionsParamsSchema = z.object({
+    id: z.string().uuid()
+})
+
+export const modifierOptionsUpdateInputSchema = z.object({
+    id: z.string().uuid(),
+    data: z.array(modifierOptionItemSchema).min(1)
+})
+
+export type ModifierOptionsInput = z.infer<typeof modifierOptionsInputSchema>
+export type ModifierOptionsOutput = z.infer<typeof modifierOptionsOutputSchema>
+export type ModifierOptionsResponseList = z.infer<typeof modifierOptionsResponseListSchema>
+export type ModifierOptionsParams = z.infer<typeof modifierOptionsParamsSchema>
+export type ModifierOptionsUpdateInput = z.infer<typeof modifierOptionsUpdateInputSchema>
+
